96ce1475a1f02ffc558da13066608dd09d08fcd2
carlos29h <carlos.29.h@gmail.com>
2023-02-10 22:28:30 -0300

    exemplos da aula 1 de Js

---
+++ types.js
@@ -0,0 +1,66 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 10;
+const segundoNumero = 0;
+
+const operaçãoMatemática = primeiroNumero / segundoNumero
+
+console.log(operaçãoMatemática)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+//NaN - Not a number (não é um numero)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero)
+
+//Tipo string
+
+const texto1 = "Olá, mundo!";
+const stringDeNumeros = "12345";
+
+// concatenação (+)
+const citacao = "Meu nome é ";
+const meuNome = "Carlos";
+
+console.log(citacao + meuNome);
+
+
+const cifrao = '\u1F43'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+// Booleanos
+const cidade = "Belo Horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const firstNumber = 5;
+const secNumber = 5;
+let cadastroAtivado = true;
+
+console.log(firstNumber === secNumber);
+
+const text1 = "Alura";
+const text2 = "a";
+
+console.log(text1 === text2)
+
+//template string ou template literal
+
